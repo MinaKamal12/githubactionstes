82ba240d6e375f770d89ad22b4097c09da3206a6
MinaKamal12 <minakamal2455@gmail.com>
2025-09-23 11:51:07 +0300

    update push-pr.yml

--- src/test.sh
+++ src/test.sh
@@ -2,14 +2,20 @@
 
 #src/test.sh
 
-EXPECTED= "Hello, test"
+EXPECTED= "Hello, test!"
 
-output = $(node -e "console.log(require('./src/app')('Test'))")
+output= $(node -e "console.log(require('./src/app')('Test'))")
 
 if [ "$output" == "$EXPECTED" ]; then
+
 echo "test passed"
+
 exit 0
+
 else
+
 echo "test failed! Expected '$EXPECTED' but got '$output'"
+
 exit 1
+
 fi 
